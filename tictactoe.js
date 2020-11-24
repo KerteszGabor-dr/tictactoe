dbe85c60a40c0dfe16b10baa3d22a46bf7530cbf
KerteszGabor-dr <kertesz.gabor.dr@gmail.com>
2020-11-24 10:31:01 +0100

    1.

---
+++ tictactoe.js
@@ -0,0 +1,14 @@
+tictactoe.js
+const matrix = [
+    ['O', 'O', 'X'],
+    ['O', null, null],
+    ['O', null, null],
+];
+
+// console.log(matrix[2][1]);
+
+for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+        console.log(matrix[i][j]);
+    }
+}
